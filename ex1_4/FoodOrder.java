1f5b9c7342a3ca4246943940677a38245aeecd8d
K K KAVIN <71024662+kkkavin@users.noreply.github.com>
2024-12-02 19:29:28 +0530

    Update FoodOrder.java

--- ex1_4/FoodOrder.java
+++ ex1_4/FoodOrder.java
@@ -5,13 +5,23 @@ public class FoodOrder {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter type of food (V/N):");
         char foodType = scanner.next().charAt(0);
+        foodType = Character.toUpperCase(foodType);
         System.out.println("Enter the quantity:");
         int quantity = scanner.nextInt();
         System.out.println("Enter the distance:");
         int distance = scanner.nextInt();
 
         double costPerPlate = foodType == 'V' ? 12 : 15;
-        double deliveryCharge = distance > 3 ? 1.5 * (distance - 3) : 0;
+        double deliveryCharge;
+        if (distance > 3 && distance <= 6){
+            deliveryCharge = distance - 3;
+        }
+        else if (distance > 6){
+            deliveryCharge = 3 + 2 * (distance - 6);
+        }
+        else{
+            deliveryCharge = 0;
+        }
         double total = quantity * costPerPlate + deliveryCharge;
 
         if (foodType != 'V' && foodType != 'N' || quantity < 1 || distance <= 0) {
